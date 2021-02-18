4ba73cfa821ef75c2a21bc89e269015367d9f77d
Katie Vogel <kvog983@gmail.com>
2021-02-17 21:47:46 -0500

    initial steps rps

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -3,21 +3,18 @@
             [main.even-or-odd]
             [main.fizzbuzz]
             #_[main.go-engine]
-            #_[main.r-p-s]
+            [main.r-p-s]
             #_[main.tic-tac-toe]))
 
-(defn init! [game-id x]
-  (println "args:" x)
-  #_(main.calculator)
-
+(defn init! [game-id x y]
+  #_(println "args:" x)
   (case game-id
     "even-or-odd"
     (println (main.even-or-odd/is-even-or-odd x))
     "fizzbuzz"
-    (println (main.fizzbuzz/fizzbuzz x)))
-
-  #_(println (main.even-or-odd/is-even-or-odd x))
-
-  #_(main.go-engine)
-  #_(main.r-p-s)
-  #_(main.tic-tac-toe))
+    (println (main.fizzbuzz/fizzbuzz x))
+    "rps"
+    (println (main.r-p-s/rps x y))
+    #_(main.calculator)
+    #_(main.go-engine)
+    #_(main.tic-tac-toe)))
--- src/main/r_p_s.cljs
+++ src/main/r_p_s.cljs
@@ -1,1 +1,21 @@
 (ns main.r-p-s)
+
+(def score
+  {"rock" {"rock" :tie
+           "paper" :two
+           "scissors" :one}
+   "paper" {"rock" :one
+            "paper" :tie
+            "scissors" :two}
+   "scissors" {"rock" :tie
+               "paper" :two
+               "scissors" :one}})
+
+(defn rps [x y]
+  (case (get (get score x) y)
+    :tie (str "Tie! Both players threw " x)
+    :one (str "Player 1 wins!" x " beats " y)
+    :two (str "Player 2 wins!" y " beats " x)))
+
+
+
